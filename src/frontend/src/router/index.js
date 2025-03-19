ac0b963be348559f4176c988d9669df7401f376d
Rudy Madrigal <madrigalrudy34@gmail.com>
2025-03-18 23:16:47 -0700

    Secured the api endpoints. No more hackers :)

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -2,11 +2,19 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Login from '../components/Login.vue';
 import Signup from '../components/Signup.vue';
 import PostPage from "@/components/PostPage.vue";
+import { isTokenValid } from '../utils/jwt'
 
 const routes = [
   { path: '/Login', name: 'Login', component: Login },
   { path: '/Signup', name: 'Signup', component: Signup },
-  {path: '/PostPage', name: 'PostPage', component: PostPage},
+  {path: '/PostPage', name: 'PostPage', component: PostPage,
+      beforeEnter: (to, from, next) =>{
+    if (!isTokenValid()){
+      next('/Login')
+    }else {
+      next();
+    }
+      }},
   { path: '/', redirect: '/Signup' }, // Redirect root to /Signup
 ];
 
