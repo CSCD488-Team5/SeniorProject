c6fd65d31582f02b1591aa1e1f770d56b192623c
Sergei Ashikhmin <sashikhmin@ewu.edu>
2025-03-21 22:03:45 -0700

    Event cards with some demo

---
+++ src/frontend/src/components/EventCard.vue
@@ -0,0 +1,52 @@
+<template>
+	<v-card class="mx-auto">
+	  <v-img height="200px" :src="imageSrc" cover></v-img>
+	  <v-card-title class="title-wrap">{{ title }}</v-card-title>
+	  <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
+  
+	  <v-card-actions>
+		<v-btn color="orange-darken-3" text variant="tonal">Explore</v-btn>
+		<v-spacer></v-spacer>
+		<v-btn icon @click="toggleContent">
+		  <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+		</v-btn>
+	  </v-card-actions>
+  
+	  <v-expand-transition>
+		<div v-show="show">
+		  <v-divider></v-divider>
+		  <v-card-text>
+			{{ content }}
+		  </v-card-text>
+		</div>
+	  </v-expand-transition>
+	</v-card>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  const props = defineProps({
+	imageSrc: { type: String, required: true },
+	title: { type: String, required: true },
+	subtitle: { type: String, required: true },
+	content: { type: String, required: true }
+  })
+  
+  const show = ref(false)
+  const toggleContent = () => {
+	show.value = !show.value
+  }
+  </script>
+  
+  <style scoped>
+  .fixed-width-card {
+	width: 250px;
+  }
+
+  .title-wrap {
+	white-space: normal;
+	overflow: visible;
+	word-wrap: break-word;
+  }
+  </style>
