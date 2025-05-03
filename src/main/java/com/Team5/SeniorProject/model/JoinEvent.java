933664a7fd15ddaebd12173e26987d79def60b2d
Rudy Madrigal <rudymadrigal@MacBookAir-129.daviscomm.net>
2025-05-02 20:23:31 -0700

    Create JoinEvent Entity

---
+++ src/main/java/com/Team5/SeniorProject/model/JoinEvent.java
@@ -0,0 +1,24 @@
+package com.Team5.SeniorProject.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "join_events")
+@Data
+
+public class JoinEvent {
+    
+}
