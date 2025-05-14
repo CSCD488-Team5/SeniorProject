850618aeda91f8b321e4b23628013eb6ecdceafc
Sergei Uss <sashikhmin@ewu.edu>
2025-05-13 18:28:49 -0700

    getDistinctCategories API

--- src/main/java/com/Team5/SeniorProject/controller/EventController.java
+++ src/main/java/com/Team5/SeniorProject/controller/EventController.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +22,6 @@ import com.Team5.SeniorProject.model.Event;
 import com.Team5.SeniorProject.model.User;
 import com.Team5.SeniorProject.repository.EventRepository;
 import com.Team5.SeniorProject.repository.UserRepository;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 import com.Team5.SeniorProject.service.EmailService;
 
 
@@ -170,8 +168,10 @@ public class EventController {
         return ResponseEntity.noContent().build();// Deletes the event.
     }
 
-    
-    
-    
+    @GetMapping("/categories")
+    public ResponseEntity<List<String>> getUniqueCategories() {
+        List<String> categories = eventRepository.findDistinctCategories();
+        return ResponseEntity.ok(categories);
+    }
 
 }
